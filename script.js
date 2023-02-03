2d6df6c4c2db2ef0dccd4b3d41a28c24f7573f9f
git.assignment-1 <cesskui4034@gmail.com>
2023-02-03 11:08:38 +0300

    commit files

---
+++ script.js
@@ -0,0 +1,16 @@
+function fetch()
+console.log(fetch("htpp://dumyjson.com)/todos")
+
+.then( res => res.jonson)())
+if(res.ok) {
+    console.log ('sucessfull')
+} else{
+   console.log ("not sucessful")
+}
+then(data =>console.log(data))
+.catch(error => console.log('error'))
+const respond= await fetch(url);
+
+
+
+}
